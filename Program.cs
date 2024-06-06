9d03a40e8d63fac802923befa10297c182d7d37c
Alain Léglise <fraternite76-alain@yahoo.fr>
2024-06-06 21:20:37 +0200

    feat: Add Program + project files

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Serilog;
+
+namespace LogMeThat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("log.txt",
+                    rollingInterval: RollingInterval.Day,
+                    rollOnFileSizeLimit: true)
+                .CreateLogger();
+
+            try
+            {
+                // Your program here...
+                const string name = "Serilog";
+                Log.Information("Hello, {Name}!", name);
+                throw new InvalidOperationException("Oops...");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Unhandled exception");
+            }
+            finally
+            {
+                Log.CloseAndFlush(); // ensure all logs written before app exits
+            }
+        }
+    }
+}
